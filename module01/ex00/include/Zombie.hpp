aad691f2f04b68d5064f547cdf6713c0a2129cb2
Guilhemdk <gdekerdanet@gmail.com>
2025-01-10 12:40:38 +0100

    fixed makefile

---
+++ module01/ex00/include/Zombie.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Zombie {
+private:
+    std::string name;
+
+public:
+    Zombie();
+
+    void announce() const;
+};
---
+++ module01/ex00/src/Zombie.cpp
@@ -0,0 +1,4 @@
+#include "../include/Zombie.hpp"
+#include <iostream>
+
+
